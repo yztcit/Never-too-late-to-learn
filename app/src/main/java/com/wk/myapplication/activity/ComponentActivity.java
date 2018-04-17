a4348dc285b42b849736a63ea06e47573b47e894
Chen_Apple <2596524106@qq.com>
2018-04-17 10:47:06 +0800

    Project start
    self-learn according a  CSDN blog

---
+++ app/src/main/java/com/wk/myapplication/activity/ComponentActivity.java
@@ -0,0 +1,15 @@
+package com.wk.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wk.myapplication.R;
+
+public class ComponentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_component);
+    }
+}
---
+++ app/src/main/java/com/wk/myapplication/activity/StyleActivity.java
@@ -0,0 +1,15 @@
+package com.wk.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wk.myapplication.R;
+
+public class StyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_style);
+    }
+}
